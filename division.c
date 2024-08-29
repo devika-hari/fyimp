e3d86a5a026c14f1d369767f207e43cca6cfbdbb
devika-hari <aesreehari@gmail.com>
2024-08-29 11:00:00 +0530

    Create division.c

---
+++ division.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+int num1,num2,num3,division;
+printf("enter any 3 number : \n");
+scanf("%d%d%d",&num1,&num2,&num3);
+division=num1/num2/num3;
+printf("%d",division);
+}
